2af46559aad7409e0b53bcfd7d6cc53ee5e2323b
niccolosea <niccolosea@gmail.com>
2025-06-20 23:33:47 +0400

    styled structure

--- partials/header.php
+++ partials/header.php
@@ -17,7 +17,7 @@ $isLoggedIn = isset($_SESSION['user']);
     />
     <link rel="stylesheet" href="css/style.css" />
     <title>Caffeine</title>
-<link rel="icon" href="img/caffeine_icon.ico" type="image/x-icon">
+    <link rel="icon" href="img/caffeine_icon.ico" type="image/x-icon">
   </head>
   <body>
     <header class="header">
